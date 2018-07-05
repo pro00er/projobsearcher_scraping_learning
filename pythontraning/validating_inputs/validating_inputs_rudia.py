75c19872760ca15ec0c997eedba2df8bc28ba20f
Rudia Moon <impactyoung@gmail.com>
2018-07-06 00:26:49 +0900

    Update validating_inputs_rudia.py
    - input에서 enter로 메소드이름 컨벤션
    - else remove

--- pythontraning/validating_inputs/validating_inputs_rudia.py
+++ pythontraning/validating_inputs/validating_inputs_rudia.py
@@ -11,36 +11,31 @@ class inputVlaueCheck(object):
             print("The "+label+" name must be filled in")
         elif len(answer) == 1:
             print("\""+answer+"\" is not a valid "+label+ " name. It is too short.")
-        else:
-            pass
 
-    def input_name(self, label):
+    def enter_name(self, label):
         a = input("Enter the "+label+" name:")
         self.check_value_name(label, a)
 
-    def input_zip_code(self):
+    def enter_zip_code(self):
         answer = input("Enter the ZIP code:")
         p = re.compile('^[0-9]*$')
         m = p.match(answer)
-        if m:
-            pass
-        else:
+        if not m:
             print('The ZIP code must be numeric:' + answer)
 
-    def input_employee_id(self):
+    def enter_employee_id(self):
         answer = input("Enter the Employee ID:")
         p = re.compile('([a-zA-Z]{1,2})\-([0-9]{4})')
         m = p.match(answer)
-        if m:
-            pass
-        else:
+        if not m:
             print(answer+' is not a valid ID')
 
 
 class excute_init(object):
     if __name__ == "__main__":
         inputVal = inputVlaueCheck()
-        inputVal.input_name("first")
-        inputVal.input_name("last")
-        inputVal.input_zip_code()
-        inputVal.input_employee_id()
+        inputVal.enter_name("first")
+        inputVal.enter_name("last")
+        inputVal.enter_zip_code()
+        inputVal.enter_employee_id()
+
