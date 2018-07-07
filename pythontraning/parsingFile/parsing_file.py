bbb95414691f69f56434aa284a48a778373ed32e
ohahohah <ohahohah.dev@gmail.com>
2018-07-07 12:53:46 +0900

    연습문제42  테스트케이스 변경 - 기존 테스트케이스 구현 - 설계 구체화 - 테스트케이스 분류, 구체화

--- pythontraning/parsingFile/parsing_file.py
+++ pythontraning/parsingFile/parsing_file.py
@@ -14,3 +14,21 @@ class ParsingFile(object):
 
     def get_salary_value(self, file):
         return file.split(",")[2]
+
+    def get_column_length(self, file, row_num):
+        return len(file.split("\n")[row_num])
+    #TODO 열에서 가장 긴단어
+
+    def get_first_column_value(self, file_multiple_row):
+        row_array = file_multiple_row.split("\n")
+
+        for row in row_array:
+            print(row.split(",")[0])
+
+    def get_column_max_word_length(self, file_multiple_row, row_num):
+        target_row = file_multiple_row.split("\n")[row_num]
+        target_words = target_row.split(",")
+        self.get_max_word_length(target_words)
+
+    def get_max_word_length(self, words):
+        return len(max(words, key=len))
--- pythontraning/parsingFile/parsing_file_test.py
+++ pythontraning/parsingFile/parsing_file_test.py
@@ -6,6 +6,7 @@ class ParsingFileTest(unittest.TestCase):
     def setUp(self):
         self.pf = ParsingFile()
         self.file = "Ling,Mai,55900"
+        self.file_multiple_row = "Ling,Mai,55900\n Johnson,Jim,56500\nZarnecki,Sabrina,51500"
 
     def test_is_exist_comma_parsing(self):
         self.assertEqual(self.pf.parsing(self.file), ["Ling","Mai","55900"])
@@ -18,5 +19,21 @@ class ParsingFileTest(unittest.TestCase):
 
     def test_success_salary_field_value(self):
         self.assertEqual(self.pf.get_salary_value(self.file), "55900")
+
+    def test_get_column_max_word_length(self):
+        self.pf.get_column_max_word_length(self.file_multiple_row, 0)
+
+# 결과값 확인이 아니라 파싱하고 있었음
+
+    def test_각_열길이가_열에서_가장_긴_단어_더하기_공백_한글자인지_확인(self):
+        row_num = 2
+        self.assertEqual(self.pf.get_column_max_word_length(self.file_multiple_row,row_num), len("Zarnecki")+1)
+
+    # def test_첫번째_열의_단어들_가져오는지_확인(self):
+    #     self.pf.get_first_column_value(self.file_multiple_row)
+        # self.assertEqual(self.pf.get_first_column_value(self.file_multiple_row), "Ling,Johnson,Zarnecki")
+
+
+
 
 
