b5660ecc83f761cde7370fe43b014ca9034052c7
ohahohah <ohahohah.dev@gmail.com>
2018-08-10 21:47:51 +0900

    Django를 시작했다

---
+++ practiceDjango/practice20180810/views.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+
+class DanceView(View):
+    def get(self, request):
+        return HttpResponse('Hello World')
